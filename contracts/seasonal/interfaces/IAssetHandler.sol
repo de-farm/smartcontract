2774e0800f81a24bf5e0f828df8d0aa130f0a720
htiennv <tien.nv291997@gmail.com>
2023-12-29 15:55:50 +0700

    Cleanup contracts for release.

--- contracts/seasonal/interfaces/IAssetHandler.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNKNOWN
-pragma solidity ^0.8.0;
-pragma experimental ABIEncoderV2;
-
-interface IAssetHandler {
-    function priceAggregators(address asset) external view returns (address);
-    function getUSDPrice(address asset) external view returns (uint256);
-}
--- contracts/seasonal/interfaces/IManaged.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNKNOWN
-pragma solidity ^0.8.0;
-
-interface IManaged {
-  function manager() external view returns (address);
-
-  function isMemberAllowed(address member) external view returns (bool);
-}
